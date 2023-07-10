769a77e8340351236d43b22a40cfbfc74d0c9c18
kimtaehoonDev <thkimm.dev@gmail.com>
2023-07-10 22:05:15 +0900

    11724/리팩토링/BufferedReader&StringTokeniner 사용

--- src/main/java/org/example/baekjoon/p11724/Main.java
+++ src/main/java/org/example/baekjoon/p11724/Main.java
@@ -1,24 +1,29 @@
 package org.example.baekjoon.p11724;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int E = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
 
         // 그래프 생성
         boolean[][] graph = new boolean[N+1][N+1];
+        boolean[] isVisited = new boolean[N+1]; // 인덱스0 사용안함
 
         for(int i=0; i<E;i++) {
-            int start = sc.nextInt();
-            int end = sc.nextInt();
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
             graph[start][end] = true;
             graph[end][start] = true;
         }
 
-        boolean[] isVisited = new boolean[N+1]; // 인덱스0 사용안함
         int answer = 0;
         for(int i=1; i<N+1;i++) {
             if (isVisited[i]) {

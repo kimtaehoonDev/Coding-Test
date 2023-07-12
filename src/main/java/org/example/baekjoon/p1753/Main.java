b132bf87d1aa57819f469ac337267ee409488acb
kimtaehoonDev <thkimm.dev@gmail.com>
2023-07-12 09:53:16 +0900

    1753/답지보고구현/우선순위큐 사용

--- src/main/java/org/example/baekjoon/p1753/Main.java
+++ src/main/java/org/example/baekjoon/p1753/Main.java
@@ -3,33 +3,32 @@ package org.example.baekjoon.p1753;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-// TODO 내일 일어나서 우선순위 큐와 함께 풀어보기
 public class Main {
-    public static int INF = 100;
+    public static int INF = 100_000_000;
+
+    static int V, E, K;
+    static int[] answer;
+    static List<Node>[] graph;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int V = Integer.parseInt(st.nextToken());
-        int E = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(br.readLine());
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(br.readLine());
 
-        boolean[] visited = new boolean[V + 1];
+        answer = new int[V+1];
+        Arrays.fill(answer, INF);
 
-        int[] answer = new int[V + 1];
-        for (int i = 2; i < V + 1; i++) {
-            answer[i] = INF;
-        }
-        answer[K] = 0;
-
-        List<List<Node>> graph = new ArrayList<>(V + 1);
+        graph = new LinkedList[V + 1];
         for (int i = 0; i < V + 1; i++) {
-            graph.add(new LinkedList<>());
+            graph[i] = new LinkedList<>();
         }
 
         // 그래프를 초기화한다
@@ -38,36 +37,10 @@ public class Main {
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
             int cost = Integer.parseInt(st.nextToken());
-            graph.get(start).add(new Node(end, cost));
+            graph[start].add(new Node(end, cost));
         }
 
-        // 가장 cost가 낮고 방문하지 않은 녀석을 넣는다
-        for (int i = 0; i < V; i++) {
-            visited[K] = true;
-            List<Node> connected = graph.get(K);
-
-            for (Node each : connected) {
-                int oldCost = answer[each.getIdx()];
-                int newCost = answer[K] + each.getCost();
-                if (newCost < oldCost) {
-                    answer[each.getIdx()] = newCost;
-                }
-            }
-
-            int min = INF;
-            Integer minIdx = null;
-            for (int j = 0; j < answer.length; j++) {
-                if (!visited[j] && answer[j] < min) {
-                    min = answer[j];
-                    minIdx = j;
-                }
-            }
-            if (minIdx == null) {
-                break;
-            }
-            K = minIdx;
-        }
-
+        dijkstra(K);
 
         // 출력
         StringBuilder sb = new StringBuilder();
@@ -81,7 +54,32 @@ public class Main {
         System.out.println(sb);
     }
 
-    static class Node {
+    private static void dijkstra(int start) {
+        boolean[] visited = new boolean[V + 1];
+
+        PriorityQueue<Node> queue = new PriorityQueue<>();
+        queue.add(new Node(start, 0));
+        answer[start] = 0;
+
+        while(!queue.isEmpty()) {
+            Node currentNode = queue.poll();
+            int current = currentNode.end;
+
+            if (visited[current]) {
+                continue;
+            }
+            visited[current] = true;
+            for(Node node: graph[current]) {
+                if (answer[node.end] > answer[current] + node.cost) {
+                    answer[node.end] = answer[current] + node.cost;
+                    queue.add(new Node(node.end, answer[node.end]));
+                }
+            }
+
+        }
+    }
+
+    static class Node implements Comparable<Node> {
         private int end;
         private int cost;
 
@@ -96,6 +94,17 @@ public class Main {
 
         public int getCost() {
             return cost;
+        }
+
+
+        @Override
+        public int compareTo(Node o) {
+            if (this.cost > o.cost) {
+                return 1;
+            } else if (this.cost < o.cost) {
+                return -1;
+            }
+            return 0;
         }
     }
 }

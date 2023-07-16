bf013bf646aa65ce0d57bef8ebe5328578a137f9
kimtaehoonDev <thkimm.dev@gmail.com>
2023-07-16 11:59:45 +0900

    1300/포기/아이디어가 떠오지 않음. 답을 봤으니 추후에 다시 도전

---
+++ src/main/java/org/example/baekjoon/p1300/Main.java
@@ -0,0 +1,12 @@
+package org.example.baekjoon.p1300;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        // TODO 실패
+    }
+}

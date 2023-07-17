693905d2ad5dd1d14a53dfc33477aaa2f1a9e52a
kimtaehoonDev <thkimm.dev@gmail.com>
2023-07-17 19:26:53 +0900

    14501/리팩토링/점화식 더 간단하게 변경

--- src/main/java/org/example/baekjoon/p14501/Main.java
+++ src/main/java/org/example/baekjoon/p14501/Main.java
@@ -5,56 +5,33 @@ import java.io.*;
 import java.math.*;
 
 public class Main {
-    static List<Consulting> consultings;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] dp = new int[N+1];// dp[x] : x일까지 벌 수 있는 최대 이익
-        for(int i=0; i<N+1; i++) {
+
+        int[] dp = new int[N + 2];// dp[x] : x일까지 벌 수 있는 최대 이익
+        int[] due = new int[N + 1];
+        int[] cost = new int[N + 1];
+
+        for (int i = 0; i < N; i++) {
             dp[i] = -1;
         }
         StringTokenizer st;
-        consultings = new ArrayList<>(N);
-        for(int start=1; start<N+1; start++) {
+        for (int start = 1; start < N + 1; start++) {
             st = new StringTokenizer(br.readLine());
-
-            int due = Integer.parseInt(st.nextToken());
-            int cost = Integer.parseInt(st.nextToken());
-
-            consultings.add(new Consulting(start, start + due - 1, cost));
-        }
-        dp[0] = 0;
-        for(int day = 1; day < N+1; day++) { //1일부터 N일까지
-            int max = 0;
-            for(int i = 0; i < day; i++) {
-                int cost = findMaxCost(i, day); // i+1일부터 day까지 벌 수 있는 단일 최대 금액
-                max = Math.max(max, dp[i] + cost);
-            }
-            dp[day] = max;
+            due[start] = Integer.parseInt(st.nextToken());
+            cost[start] = Integer.parseInt(st.nextToken());
         }
 
-        System.out.println(dp[dp.length - 1]);
-    }
-
-    private static int findMaxCost(int start, int end) {
-        int max = 0;
-        for (Consulting consulting : consultings) {
-            if (start < (consulting.start) && consulting.end <= end) {
-                max = Math.max(max, consulting.cost);
+        for (int i = N; i > 0; i--) {
+            if (i + due[i] - 1 > N) { //범위를 벗어났을때
+                dp[i] = dp[i + 1];
+            } else {
+                dp[i] = Math.max(dp[i + 1], dp[i + due[i]] + cost[i]);
             }
         }
-        return max;
+
+        System.out.println(dp[1]);
     }
 
-    static class Consulting {
-        int start;
-        int end;
-        int cost;
-
-        public Consulting(int start, int end, int cost) {
-            this.start = start;
-            this.end = end;
-            this.cost = cost;
-        }
-    }
 }

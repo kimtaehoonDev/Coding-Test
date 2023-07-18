cc423a83892ca8aa61e3a3812b82be38fe81e800
kimtaehoonDev <thkimm.dev@gmail.com>
2023-07-18 21:56:10 +0900

    2156/성공

---
+++ src/main/java/org/example/baekjoon/p2156/Main.java
@@ -0,0 +1,37 @@
+package org.example.baekjoon.p2156;
+
+import java.util.*;
+import java.math.*;
+import java.io.*;
+
+public class Main {
+    static int[] ary;
+    static int[] dp;
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        ary = new int[N+1];
+        dp = new int[N+1];
+        for(int i=1; i<N+1; i++) {
+            ary[i] = Integer.parseInt(br.readLine());
+        }
+        System.out.println(solve(N));
+    }
+
+    public static int solve(int n) {
+        if (n <= 0) {
+            return 0;
+        }
+        if (dp[n] != 0) {
+            return dp[n];
+        }
+        int max = -1;
+        max = Math.max(max, solve(n-3) + ary[n-1] + ary[n]);
+        max = Math.max(max, solve(n-2) + ary[n]);
+        max = Math.max(max, solve(n-1));
+        dp[n] = max;
+        return dp[n];
+
+    }
+}

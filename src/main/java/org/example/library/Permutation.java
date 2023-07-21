d3f89f23096b05aea2392f1741126871df7acb63
kimtaehoonDev <thkimm.dev@gmail.com>
2023-07-21 11:32:46 +0900

    library/순열 메서드 구현

--- src/main/java/org/example/library/Permutation.java
+++ src/main/java/org/example/library/Permutation.java
@@ -10,29 +10,26 @@ public class Permutation {
         for (int i = 0; i < 4; i++) {
             ary[i] = i;
         }
-        List<List<Integer>> permutation = permutation(ary, 2);
-        for (List<Integer> each : permutation) {
-            for (Integer integer : each) {
-                System.out.print(integer);
-            }
-            System.out.println("\n---");
+        List<int[]> permutation = permutation(ary, 2);
+        for (int[] each : permutation) {
+            System.out.println(Arrays.toString(each));
         }
         System.out.println("크기: " + permutation.size());
     }
 
     // nPr
-    public static List<List<Integer>> permutation(int[] ary, int r) {
-        List<List<Integer>> answer = new ArrayList<>();
+    public static List<int[]> permutation(int[] ary, int r) {
+        List<int[]> answer = new ArrayList<>();
         return permutationHelper(ary, r, 0, 0, answer);
     }
 
-    private static List<List<Integer>> permutationHelper(int[] ary, int r, int depth,
-                                                 int pointer, List<List<Integer>> answer) {
+    private static List<int[]> permutationHelper(int[] ary, int r, int depth,
+                                                 int pointer, List<int[]> answer) {
         int n = ary.length;
         if (r == depth) {
-            List<Integer> result = new ArrayList<>();
+            int[] result = new int[r];
             for(int i=0; i<r; i++) {
-                result.add(ary[i]);
+                result[i] = ary[i];
             }
             answer.add(result);
             return null;

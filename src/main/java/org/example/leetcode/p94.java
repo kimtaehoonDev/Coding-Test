6c505cc77e9ed116ed3576acc3e617ead891a423
kimtaehoonDev <thkimm.dev@gmail.com>
2023-08-11 11:00:36 +0900

    리트코드/p94/성공/binary-tree-inorder-traversal

---
+++ src/main/java/org/example/leetcode/p94.java
@@ -0,0 +1,43 @@
+package org.example.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.tree.TreeNode;
+
+// https://leetcode.com/problems/binary-tree-inorder-traversal
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class p94 {
+//    public List<Integer> inorderTraversal(TreeNode root) {
+//        // 트리가 존재하면
+//        List<Integer> answer = new ArrayList<>();
+//        goTree(root, answer);
+//        return answer;
+//    }
+//
+//    public void goTree(TreeNode root, List<Integer> answer) {
+//        if (root == null) {
+//            return;
+//        }
+//        if (root.left != null) {
+//            goTree(root.left, answer);
+//        }
+//        answer.add(root.val);
+//        if (root.right != null) {
+//            goTree(root.right, answer);
+//        }
+//    }
+}

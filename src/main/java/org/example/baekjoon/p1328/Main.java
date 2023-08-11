73148f710697d71ecbfecc38bf7c2a44df9e57b5
kimtaehoonDev <thkimm.dev@gmail.com>
2023-08-11 20:04:56 +0900

    백준/p1328/답지 참고 후 풀이

--- src/main/java/org/example/baekjoon/p1328/Main.java
+++ src/main/java/org/example/baekjoon/p1328/Main.java
@@ -5,78 +5,22 @@ import java.util.Scanner;
 // 어렵다. 시간 초과 날거 알았는데 다른 방법이 떠오르지 않음
 
 public class Main {
-    public static int N;
-    public static int L;
-    public static int R;
-    public static boolean[] visited;
-    public static int[] output;
-    public static int answer = 0;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        L = sc.nextInt();
-        R = sc.nextInt();
-
-        visited = new boolean[N+1];
-        output = new int[N];
-
-        solve(N, -1, 0, 0);
-        System.out.println(answer);
-    }
-
-    public static void solve(int n, int prevTop, int leftSee, int depth) {
-        // 종료조건 시 left, right 개수를 확인한다
-        if (depth == n) {
-
-            if (leftSee != L) {
-                return;
-            }
-            // n개를 뽑았다.
-            // output을 확인하며 right 개수
-            int rightTop = -1;
-            int rightSee = 0;
-            for(int i=n-1; i>=0 ;i--) {
-                if (output[i] > rightTop) {
-                    rightTop = output[i];
-                    rightSee++;
-                }
-            }
-            if (rightSee == R) {
-                // 정답추가
-                answer = (answer + 1) % 1000000007;
-            }
-            return;
-        }
-
-        for(int i=1; i<=n; i++) {
-            if (!visited[i]) {
-                // 기존 가장 높은 건물보다 높으면 보이게 된다
-                if (prevTop < i) {
-                    int temp = prevTop;
-                    prevTop = i;
-                    leftSee++;
-
-                    // 종료조건
-                    if (leftSee > L) {
-                        leftSee--;
-                        prevTop = temp;
-                        continue;
-                    }
-                    visited[i] = true;
-                    output[depth] = i;
-                    solve(n, prevTop, leftSee, depth+1);
-                    visited[i] = false;
-                    output[depth] = 0;
-                    leftSee--;
-                    prevTop = temp;
-                } else {
-                    visited[i] = true;
-                    output[depth] = i;
-                    solve(n, prevTop, leftSee, depth + 1);
-                    visited[i] = false;
-                    output[depth] = 0;
+        int n = sc.nextInt();
+        int l = sc.nextInt();
+        int r = sc.nextInt();
+        long[][][] dp = new long[n+1][n+1][n+1];
+        dp[1][1][1] = 1;
+        for(int i=2;i<n+1;i++) {
+            for(int j=1; j<l+1;j++) {
+                for(int k=1; k<r+1;k++) {
+                    dp[i][j][k] = (dp[i-1][j][k] * (i-2)) + dp[i-1][j-1][k] + dp[i-1][j][k-1];
+                    dp[i][j][k] %= 1000000007;
                 }
             }
         }
+        System.out.println(dp[n][l][r]);
     }
 }

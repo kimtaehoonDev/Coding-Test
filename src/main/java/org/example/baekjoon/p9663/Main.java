099ae8d4ae69897254cefb147ba25c5ac1de5068
kimtaehoonDev <thkimm.dev@gmail.com>
2023-08-15 17:29:36 +0900

    백준/9663/풀이 참고/N-Queens

--- src/main/java/org/example/baekjoon/p9663/Main.java
+++ src/main/java/org/example/baekjoon/p9663/Main.java
@@ -1,91 +1,56 @@
 package org.example.baekjoon.p9663;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.Arrays;
 import java.util.Scanner;
 
-// 시간초과 -> 메모리초과(dp테이블 사용) -> 실패
+// 핵심 아이디어는 한 row에 하나의 Queen만 위치할 수 있다는 것이다.
+// 즉, 1차원 배열만을 사용해서 2차원에 존재하는 Queen들의 위치를 표현할 수 있다
+// cols[0] = 1 의 의미는 Row 0, Col 1에 Queen이 존재한다는 뜻
+
 public class Main {
     public static int answer = 0;
-    public static Boolean[][] dp;
     public static int N;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
-        dp = new Boolean[N*N][N*N];
-
-        comb(N*N, N, 0, 0, new ArrayList<>());
+        int[] cols = new int[N]; // 인덱스는 rows
+        Arrays.fill(cols, -1);
+        nQueens(0, cols);
         System.out.println(answer);
     }
 
-    public static void comb(int n, int r, int depth, int start, List<Spot> queens) {
-        if (depth == r) {
+    // queens의 인덱스 : x, queens의 값: y값
+    public static void nQueens(int targetRow, int[] cols) {
+        if (targetRow == N) {
             answer++;
             return;
         }
 
-        for(int i=start; i<n;i++) {
-            int x = i/N;
-            int y = i%N;
-            boolean canAttack = checkAttack(x, y, queens);
-            if (canAttack) {
+        // queens
+        for(int targetCol=0; targetCol<N; targetCol++) {
+            cols[targetRow] = targetCol;
+            if (canAttack(targetRow, cols)) {
                 continue;
             }
-            queens.add(Spot.get(x,y));
-            comb(n, r, depth + 1, i + 1, queens);
-            queens.remove(queens.size() - 1);
+            cols[targetRow] = targetCol;
+            nQueens(targetRow + 1, cols);
+            cols[targetRow] = -1;
         }
     }
 
-    public static boolean checkAttack(int x, int y, List<Spot> queens) {
-        for (Spot queen : queens) {
-            if (dp[queen.x * N + queen.y][x*N + y] != null) {
-                if  (dp[queen.x * N + queen.y][x*N + y]) {
-                    return true;// 공격가능
-                }
-                continue;
-            }
-
-            if (x == queen.x) {
-                dp[queen.x * N + queen.y][x*N + y] = true; // 공격가능
+    // x위치에 y를 넣어줄거다
+    public static boolean canAttack(int targetRow, int[] cols) {
+        // 다른 Queen들과 비교한다(targetRow 앞까지가 현재 선택된 케이스)
+        for (int i = 0; i < targetRow; i++) {
+            if (cols[targetRow] == cols[i]) {
                 return true;
             }
-            if (y == queen.y) {
-                dp[queen.x * N + queen.y][x*N + y] = true;
+            if (Math.abs(targetRow - i) == Math.abs(cols[targetRow] - cols[i])) {
                 return true;
             }
-            if (Math.abs(queen.x - x) == Math.abs(queen.y - y)) {
-                dp[queen.x * N + queen.y][x*N + y] = true;
-                return true;
-            }
-            dp[queen.x * N + queen.y][x*N + y] = false;
         }
         return false;
     }
 
-    public static class Spot {
-        static Map<Integer, Spot> store = new HashMap<>();
-
-        int x;
-        int y;
-        private Spot(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        static {
-            for(int i=0;i<N;i++) {
-                for(int j=0;j<N;j++) {
-                    store.put(N*i + j, new Spot(i, j));
-                }
-            }
-        }
-
-        public static Spot get(int x, int y) {
-            return store.get(N * x + y);
-        }
-    }
 }

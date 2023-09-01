464fcb8266fa373679c1af8ffea891af4106b15b
kimtaehoonDev <thkimm.dev@gmail.com>
2023-09-01 18:39:48 +0900

    프로그래머스/과제 진행하기/풀이 참조

--- src/main/java/org/example/programmers/연속된_부분_수열의_합.java
+++ src/main/java/org/example/programmers/연속된_부분_수열의_합.java
@@ -1,55 +1,58 @@
 package org.example.programmers;
 
-// 실패: 풀이시간 43분
+// 투포인터라는 생각을 못했다
+// [1,1,1,2,3,6] 이라는 값이 있다고 하자.
+// 투포인터 방식으로 풀면 1,2,3이 선택된다.
+// 나는 여기서 1,2,3을 선택한 뒤 어떻게 더 로직을 이어가야 할지 생각을 못해 투포인터는 아니라고 생각했다
+// 그러나, 단순하게도 정답이 나오고 나서 계속 진행을 하면 된다. left를 옮기건, right를 옮기건 둘 다 옮기건 아무튼 옮겨서 계속 로직을 이어가면 된다...
+
 public class 연속된_부분_수열의_합 {
     class Solution {
         public int[] solution(int[] sequence, int k) {
-            int[] answer = {};
             int start = 0;
-            int end = sequence.length; // 백만개
-
-            // 정답을 만들 수 있는 가능성이 있는 가장 작은 원소 개수는? -> start
-            while(start < end) {
-                int mid = (start + end)/2;
-                int temp = 0;
-                for (int i= sequence.length - mid; i< sequence.length; i++) {
-                    temp += sequence[i];
-                }
-                if (temp < k) {
-                    start = mid + 1;
-                } else {
-                    end = mid;
-                }
-                // end -1 부터 end - mid 까지 선택해 -> 그녀석들의 합 < k: mid개 뽑아서 정답이 될 수 없음
+            int end = 0;
+            int sum = sequence[start];
+            if (sum == k) {
+                return new int[] {0,0};
             }
 
-            // start부터 비교하며 답을 찾아나간다
-            for(int i = start; i<=sequence.length; i++) {
-                // i개를 뽑을겁니다. 슬라이딩 윈도우로 합을 구해나갑니다
-                int temp = 0;
-                for(int j=0; j<start;j++) {
-                    temp += sequence[j];
-                }
-                // System.out.println(start+"개 선택 -> 최소합 " + temp);
-                if (temp == k) {
-                    // 정답 리턴
-                    return new int[] {0, start - 1};
-                } else if (temp < k) {
-                    // 가장 앞에 한개 빼고, 뒤에 한개 넣고
-                    int idx = 0;
-                    while(idx+start < sequence.length) {
-                        temp = temp - sequence[idx] + sequence[idx + start];
-                        idx++;
-                        if (temp == k) {
-                            // 정답 리턴
-                            return new int[] {idx, idx + start - 1};
-                        }
-                    }
-                } else {
-                    // i개 뽑아서는 답이 나올 수 없음. 다음 i 경우를 확인
-                    continue;
+            int[] answer = new int[2];
+            answer[0] = -1000000000;
+            answer[1] = 1000000000;
 
+
+            while(start <= end && end < sequence.length) {
+                // start와 end 사이 합을 구한다
+                if (sum < k) {
+                    end++;
+                    if (end >= sequence.length) {
+                        break;
+                    }
+                    sum += sequence[end];
+                    continue;
                 }
+                if (sum > k) {
+                    sum -= sequence[start];
+                    start++;
+                    continue;
+                }
+                // sum == k
+
+                // 새 값이 더 짧으면
+                if ((answer[1] - answer[0]) > (end - start)) {
+                    answer[0] = start;
+                    answer[1] = end;
+                    if (start == end) {
+                        break;
+                    }
+                }
+                end++;
+                if (end >= sequence.length) {
+                    continue;
+                }
+
+                sum = sum + sequence[end] - sequence[start];
+                start++;
             }
             return answer;
         }

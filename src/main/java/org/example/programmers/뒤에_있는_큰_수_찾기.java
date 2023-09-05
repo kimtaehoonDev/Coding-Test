73241db34c562a97caf82103701c17a3ece7a11d
kimtaehoonDev <thkimm.dev@gmail.com>
2023-09-05 19:30:43 +0900

    프로그래머스/뒤에 있는 큰 수 찾기/다른 사람 아이디어 참고해 더 개선된 방식으로 변경

--- src/main/java/org/example/programmers/뒤에_있는_큰_수_찾기.java
+++ src/main/java/org/example/programmers/뒤에_있는_큰_수_찾기.java
@@ -5,47 +5,39 @@ import java.util.*;
 // 성공/31분
 // 잘 생각했고 수도코드도 잘 써서 풀긴 함
 // 수도코드를 만드는 능력이 매우 떨어져있음
+
+// 다른 사람 풀이인데, 스택을 사용하는 게 훨씬 깔끔하고 이해하기도 좋음
+// https://sasca37.tistory.com/306
 class 뒤에_있는_큰_수_찾기 {
     public int[] solution(int[] numbers) {
-
         int[] answer = new int[numbers.length];
-        int[] answerIdx = new int[numbers.length];
         Arrays.fill(answer, -1);
 
-        for(int i=numbers.length-1; i>=0 ; i--) {
-            if (i==numbers.length - 1) {
-                answerIdx[i] = -1; // 여기까지 왔다는 건 뒷큰수가 없다는 거
-                continue;
-            }
-
-            // 바로 뒤 값이 더 크면 -> i+1이 뒤큰수가 됨
-            if (numbers[i] < numbers[i+1]) {
-                answer[i] = numbers[i+1];
-                answerIdx[i] = i+1;
-                continue;
-            }
-
-            int tempIdx = i + 1;
-            while (true) {
-                if (answer[tempIdx] == -1) {
-                    answer[i] = -1;
-                    answerIdx[i] = -1; // 뒤큰수 없다
-                    break;
+        Stack<Node> stack = new Stack<>();
+        for(int i=0; i<numbers.length;i++) {
+            while (!stack.isEmpty()) {
+                Node top = stack.peek();
+                if (top.value < numbers[i]) {
+                    answer[top.idx] = numbers[i];
+                    stack.pop();
+                    continue;
                 }
-                if (answer[tempIdx] > numbers[i]) {
-                    // answer[tempIdx]가 i의 뒤큰수가 됨
-                    answer[i] = answer[tempIdx];
-                    answerIdx[i] = tempIdx;
-                    break;
-                }
-
-                tempIdx = answerIdx[tempIdx];
+                break;
             }
 
-
-
+            stack.push(new Node(i, numbers[i]));
         }
 
         return answer;
+    }
+
+    static class Node {
+        int idx;
+        int value;
+
+        public Node(int idx, int value) {
+            this.idx = idx;
+            this.value = value;
+        }
     }
 }

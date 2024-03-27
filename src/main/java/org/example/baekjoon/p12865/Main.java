a46badd37d7383b2471df618115673486f56ccc0
kimtaehoonDev <thkimm.dev@gmail.com>
2024-03-27 21:58:55 +0900

    백준/12865/답안 참조 후 풀이/배낭문제

--- src/main/java/org/example/baekjoon/p12865/Main.java
+++ src/main/java/org/example/baekjoon/p12865/Main.java
@@ -3,10 +3,8 @@ package org.example.baekjoon.p12865;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
-// 실패 -> 시간초과 / 1시간 10분 고민
-// 가지치기로 풀려 했으나 실패함. 분명 배웠던 내용인데 기억이 안남
+// 답안 참조 후 풀이 -> 물건을 쪼갤 수 없는 배낭 문제는 DP로 푼다.
 public class Main {
 
     static int WEIGHT = 0;
@@ -21,38 +19,28 @@ public class Main {
         N = Integer.parseInt(input[0]);
         K = Integer.parseInt(input[1]);
 
-        int[][] products = new int[N][2];
-        for(int i =0; i<N; i++) {
+        int[] weights = new int[N + 1];
+        int[] values = new int[N + 1];
+        int[][] dp = new int[N + 1][K + 1];
+
+        for(int i =1; i<=N; i++) {
             input = br.readLine().split(" ");
-            products[i][WEIGHT] = Integer.parseInt(input[0]);
-            products[i][VALUE] = Integer.parseInt(input[1]);
+            weights[i] = Integer.parseInt(input[0]);
+            values[i] = Integer.parseInt(input[1]);
         }
 
-        Arrays.sort(products, (a1, a2) -> {
-            if (a1[WEIGHT] != a2[WEIGHT]) {
-                return a1[WEIGHT] - a2[WEIGHT]; // 무게가 낮은 순서로 정렬
+        for(int i =1; i<=N; i++) {
+            for(int j = 1; j<=K; j++) {
+                if (weights[i] > j) {
+                    // i번째 물건을 담을 수 없다
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    // i번째 물건을 담을 수 있다.
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - weights[i]] + values[i]);
+                }
             }
-            return a2[VALUE] - a1[VALUE]; // 무게가 같으면 가치가 높은게 우선순위 O
-        });
-//        for (int[] product : products) {
-//            System.out.println(Arrays.toString(product));
-//        }
-
-        int answer = calcMax(0, 0, 0, products);
-        System.out.println(answer);
-    }
-
-    static int calcMax(int depth, int weightInBackpack, int valueInBackpack, int[][] products) {
-//        System.out.println(String.format("현재 깊이 : %d, 무게 : %d, 가치 : %d", depth, weightInBackpack, valueInBackpack));
-        int max = valueInBackpack;
-        for(int i =depth; i<N; i++) {
-            if (weightInBackpack + products[i][WEIGHT] > K) {
-                break;
-            }
-            int temp = calcMax(i + 1, weightInBackpack + products[i][WEIGHT], valueInBackpack + products[i][VALUE], products);
-            max = Math.max(max, temp);
         }
-        return max;
+        System.out.println(dp[N][K]);
     }
 
 }

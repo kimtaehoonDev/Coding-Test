f53424f39ccf9a869b0e50f3ac36719a90962257
kimtaehoonDev <thkimm.dev@gmail.com>
2024-05-02 20:25:14 +0900

    프그/카페 확장(PCCP 모의고사 2 - 3번)/실패 후 성공/구현

--- src/main/java/org/example/programmers/pccp/round2/Num3.java
+++ src/main/java/org/example/programmers/pccp/round2/Num3.java
@@ -1,51 +1,73 @@
 package org.example.programmers.pccp.round2;
 
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
 
-// 실패
+// 다시 풀이 후 성공 / 구현 / 22분
+// 뭐가 문제였을까.. 아무래도 큐 하나 안쓰고 괜히 머리쓴다고 설쳤다가 틀린 거 같다
+// 쉬운?? 문제인데 틀린게 아쉽다.
 class Num3 {
+
     public int solution(int[] menu, int[] order, int k) {
-        Queue<Integer> waitings = new LinkedList<>();
+        Queue<Integer> completes = new LinkedList<>(); // 손님이 만들어달라 요청한 음료 완성 시간들
 
-        int barister = 0;
-        int answer = 0;
+        Queue<Customer> customers = new LinkedList<>(); // 손님들이 들어오는 초
+        for (int i = 0; i < order.length; i++) {
+            customers.add(new Customer(k * i, order[i]));
+        }
+        Queue<Customer> waitings = new LinkedList<>();
+
+        // System.out.println(customers);
         int time = -1;
-        int orderIdx = 0;
+        int barister = 0;
         int peopleCnt = 0;
-
-        // 모든 손님이 주문 끝나고 나가면
-        while(true) {
+        int answer = 0;
+        while (true) {
             time++;
 
-            // 나갈 손님은 나간다(음료를 다 만들었다)
-            if (barister <= time && barister != 0) {
+            // 나갈 손님 나간다
+            if (!completes.isEmpty() && completes.peek() == time) {
                 peopleCnt--;
+                completes.poll();
             }
 
-            // 새로 들어온 손님은 주문한다.
-            if (time % k == 0 && (time / k) < order.length) { // 일정한 시간에 손님이 들어와서 주문을 한다.
-                waitings.add(order[time / k]);
+            // 들어올 손님 들어온다
+            if (!customers.isEmpty() && customers.peek().time == time) {
                 peopleCnt++;
+                // 주문한다.
+                Customer entered = customers.poll();
+                waitings.add(entered);
             }
 
-            // 주문 밀려있으면 바리스타는 바로 음료를 제작한다.
-            if (barister <= time && !waitings.isEmpty()) {
-                int makingMenu = waitings.poll();
-                int makingTime = menu[makingMenu];
-                barister += makingTime;
+            // 음료 만들 수 있으면 음료 만든다.
+            if (!waitings.isEmpty() && barister <= time) {
+                Customer customer = waitings.poll();
+                int makingTime = menu[customer.productNum];
+                barister = time + makingTime;
+                completes.add(barister);
             }
 
-            // 사람 숫자를 센다
+            // System.out.println(time + "초 사람숫자-> " + peopleCnt);
             answer = Math.max(peopleCnt, answer);
-            // System.out.println(time + "초에 총 사람" + peopleCnt);
-            // System.out.println("바리스타는 일하는중 몇초까지? -> " + barister);
 
-            // 모든 음료 제작에 끝나면(바리스타 쉬고 있고, 손님은 다 받았고)
-            if (barister <= time && (time / k) > order.length - 1) {
+            // 들어올 손님 없고, 나갈 손님 없으면 끝
+            if (customers.isEmpty() && completes.isEmpty()) {
                 break;
             }
         }
 
         return answer;
     }
+
+    static class Customer {
+
+        int time;
+        int productNum;
+
+        public Customer(int time, int productNum) {
+            this.time = time;
+            this.productNum = productNum;
+        }
+    }
+
 }
